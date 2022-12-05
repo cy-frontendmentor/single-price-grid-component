78c3f67599fc6f6ab923d97558dca94ca782083c
CY Chan <itroychan@gmail.com>
2022-12-05 20:03:05 +0800

    Updated responsice

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 const page = () => {
   return (
-    <div className=" max-w-[635px] rounded-lg px-8 drop-shadow-[0_15px_30px_rgba(0,81,171,0.1488)]">
-      <div className="grid">
-        <div className="bg-white px-6 pt-7 pb-8">
+    <div className=" max-w-[635px] px-8 drop-shadow-[0_15px_30px_rgba(0,81,171,0.1488)] md:px-0">
+      <div className="grid overflow-hidden rounded-lg  ">
+        <div className="  bg-white px-6 pt-7 pb-8 md:p-10 ">
           <h1 className=" mb-6 text-xl font-bold -tracking-[0.25px] text-cyan">
             Join our community
           </h1>
-          <p className=" mb-4 text-[15px] font-bold leading-5 -tracking-[0.19px] text-yellow-bright">
+          <p className=" mb-4 text-[15px] font-bold leading-5 -tracking-[0.19px] text-yellow-bright md:mb-[11px]">
             30-day, hassle-free money back guarantee
           </p>
           <p className=" text-sm leading-[26px] -tracking-[0.17px] text-blue-grayish">
@@ -15,8 +15,8 @@ const page = () => {
             their skills.
           </p>
         </div>
-        <div className="gird bg-cyan">
-          <div className=" p-6">
+        <div className="flex flex-col bg-cyan md:flex-row  ">
+          <div className=" p-6 md:w-1/2 md:p-10">
             <h2 className=" mb-[18px] text-lg font-bold -tracking-[0.22px] text-white">
               Monthly Subscription
             </h2>
@@ -29,11 +29,11 @@ const page = () => {
             <p className=" mb-[26px] text-base  leading-[26px] -tracking-[0.2px] text-white">
               Full access for less than $1 a day
             </p>
-            <button className=" w-full rounded-[5px] bg-yellow-bright py-[14px] text-base  text-white drop-shadow-[0_15px_30px_rgba(0,0,0,0.0973)] ">
+            <button className=" w-full rounded-[5px] bg-yellow-bright py-[14px] text-base  text-white drop-shadow-[0_10px_10px_rgba(0,0,0,0.0973)] ">
               Sign Up
             </button>
           </div>
-          <div className="bg-white bg-opacity-[15.19%] p-6">
+          <div className="bg-white bg-opacity-[15.19%] p-6 md:w-1/2 md:p-10">
             <h2 className=" text-lg font-bold -tracking-[0.22px] text-white">
               Why Us
             </h2>
